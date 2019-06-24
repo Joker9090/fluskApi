d9ac541897b52487257b7da495ec351431161af8
Juan Martin Cerruti <juanmartin.cerruti@gmail.com>
2019-06-24 13:26:19 -0300

    add connection to db

--- app.py
+++ app.py
@@ -1,13 +1,30 @@
 from flask import Flask
+from flask_mysqldb import MySQL
 import psutil
 import json
+
 #print(dir(psutil))
 
 app = Flask(__name__)
 
+app.config['MYSQL_HOST'] = '127.0.0.1'
+app.config['MYSQL_PORT'] = 3306
+app.config['MYSQL_USER'] = ''
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = ''
+
+mysql = MySQL(app)
+
 @app.route('/')
 def hello():
-    return json.dumps({ 'cpu_stats': psutil.cpu_stats()})
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM exchange_symbols")
+    result = cur.fetchall()
+    items = []
+    for row in result:
+        items.append({ 'id': row[0], 'internalSymbol': row[4] })
+    cur.close()
+    return json.dumps({ 'items': items })
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True, use_reloader=False)
